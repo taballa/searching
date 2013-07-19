2b71978291e90ac1199023b58356fe4923a87d29
taballa-hp-mini <taballa@gmail.com>
2013-07-19 14:56:26 +0800

    delet google custom search code.

--- index.php
+++ index.php
@@ -60,7 +60,6 @@ if ($_GET && $_GET['keyword']){
     <title>Searching</title>
     <link rel="stylesheet" href="stylesheets/main.css">
     <link rel="stylesheet" href="stylesheets/screen.css">
-    <!-- <link rel='stylesheet' href='http://www.google.com/cse/style/look/default.css' type='text/css'/> -->
     <script src="components/jquery/jquery.js"></script>
 </head>
 <body>
@@ -76,28 +75,6 @@ if ($_GET && $_GET['keyword']){
             <?php echo $baidu_content ?>
         </div> 
         <div id="google-content">
-<!--             <div id='cse' style='width: 100%;'>Loading</div>
-            <script src='//www.google.com/jsapi' type='text/javascript'></script>
-            <script type='text/javascript'>
-            google.load('search', '1', {language: 'zh-Hans', style: google.loader.themes.DEFAULT});
-            google.setOnLoadCallback(function() {
-              var customSearchOptions = {};
-              var orderByOptions = {};
-              orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
-              customSearchOptions['enableOrderBy'] = true;
-              customSearchOptions['orderByOptions'] = orderByOptions;
-              var imageSearchOptions = {};
-              imageSearchOptions['layout'] = 'google.search.ImageSearch.LAYOUT_POPUP';
-              customSearchOptions['enableImageSearch'] = true;
-              customSearchOptions['adoptions'] = {'layout' : 'noTop'};
-              var customSearchControl =   new google.search.CustomSearchControl('013485985423317541061:2qzqsth3mng', customSearchOptions);
-              customSearchControl.setResultSetSize(google.search.Search.LARGE_RESULTSET);
-              var options = new google.search.DrawOptions();
-              customSearchControl.draw('cse', options);
-              customSearchControl.execute("<?php echo $keyword_origin ?>");
-              }, true);
-            </script>
-        -->        
         <?php echo $google_content ?>
     </div>
 </div>
