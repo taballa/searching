de69a6a5d1f0f305210e44a39edaa357bea885ea
taballa-hp-mini <taballa@gmail.com>
2013-07-21 21:21:51 +0800

    append frame.php

---
+++ frame.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>Frame search</title>
+</head>
+<frameset cols="50%, 50%">
+<frame src="http://www.google.com/custom?q=abc">
+<frame src="http://www.baidu.com/">
+</frameset>
+</html>
